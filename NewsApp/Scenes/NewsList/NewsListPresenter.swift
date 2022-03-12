493783e9018fcce55be046f40707c8f644cf735e
cocobomi <ehdgus0424@naver.com>
2022-03-12 17:29:42 +0900

    NewsListScene의 NavigationBar, UITableView 구현

---
+++ NewsApp/Scenes/NewsList/NewsListPresenter.swift
@@ -0,0 +1,49 @@
+//
+//  NewsListPresenter.swift
+//  NewsApp
+//
+//  Created by donghyeon on 2022/03/12.
+//
+
+import UIKit
+
+protocol NewsListProtocol: AnyObject {
+    func setupNavigationBar()
+    func setupLayout()
+    func endRefreshing()
+}
+
+final class NewsListPresenter: NSObject {
+    private weak var viewController: NewsListProtocol?
+    
+    init(viewController: NewsListProtocol) {
+        self.viewController = viewController
+    }
+    
+    func viewDidLoad() {
+        viewController?.setupNavigationBar()
+        viewController?.setupLayout()
+    }
+    
+    func didCalledRefresh() {
+        viewController?.endRefreshing()
+    }
+}
+
+extension NewsListPresenter: UITableViewDelegate {
+    
+}
+
+extension NewsListPresenter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NewsListTableViewCell.identifier, for: indexPath) as? NewsListTableViewCell
+        
+        cell?.setup()
+        
+        return cell ?? UITableViewCell()
+    }
+}
---
+++ NewsApp/Scenes/NewsList/NewsListViewController.swift
@@ -0,0 +1,64 @@
+//
+//  NewsListViewController.swift
+//  NewsApp
+//
+//  Created by donghyeon on 2022/03/12.
+//
+
+import UIKit
+import SnapKit
+
+final class NewsListViewController: UIViewController {
+    private lazy var presenter = NewsListPresenter(viewController: self)
+    
+    private lazy var  refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(didCalledRefresh), for: .valueChanged)
+        
+        return refreshControl
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = presenter
+        tableView.dataSource = presenter
+        
+        tableView.register(NewsListTableViewCell.self, forCellReuseIdentifier: NewsListTableViewCell.identifier)
+        
+        tableView.refreshControl = refreshControl
+        
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        presenter.viewDidLoad()
+    }
+}
+
+extension NewsListViewController: NewsListProtocol {
+    func setupNavigationBar() {
+        navigationItem.title = "NEWS"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func setupLayout() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func endRefreshing() {
+        refreshControl.endRefreshing()
+    }
+}
+
+private extension NewsListViewController {
+    @objc func didCalledRefresh() {
+        presenter.didCalledRefresh()
+    }
+}
+
